5c5171d7aafaed35cf70327994b560babead303d
gucchunchun <yunataniguchi23@gmail.com>
2023-09-05 22:09:38 +0100

    Add Sketch-board Styles & Add Box/Sketch class
    *for Box/Sketch classes, I refer my previous portofolio 'random-box'

--- index.js
+++ index.js
@@ -1,2 +1,129 @@
 "use strict";
+var ColorOption;
+(function (ColorOption) {
+    ColorOption["Red"] = "red";
+    ColorOption["Green"] = "green";
+    ColorOption["Blue"] = "blue";
+    ColorOption["Alfa"] = "alfa";
+})(ColorOption || (ColorOption = {}));
+const defaultColor = {
+    [ColorOption.Red]: 255,
+    [ColorOption.Green]: 255,
+    [ColorOption.Blue]: 255,
+    [ColorOption.Alfa]: 1,
+};
+class Box {
+    constructor(_sketch, _width, _color = JSON.parse(JSON.stringify(defaultColor)), _classList = []) {
+        this._sketch = _sketch;
+        this._width = _width;
+        this._color = _color;
+        this._classList = _classList;
+        this.self = undefined;
+    }
+    add(insertBeforeElement = undefined) {
+        let self = document.createElement('div');
+        //default className
+        self.classList.add('sketch__box');
+        //additional className for style changes
+        for (const name of this._classList) {
+            self.classList.add(name);
+        }
+        self.style.width = this._width + '%';
+        self.style.backgroundColor = `rgba(${this._color[ColorOption.Red]}, ${this._color[ColorOption.Green]},${this._color[ColorOption.Blue]},${this._color[ColorOption.Alfa]})`;
+        self.addEventListener('mousedown', () => {
+            //some function to change color with specified color
+        });
+        if (insertBeforeElement) {
+            this._sketch.insertBefore(self, insertBeforeElement);
+        }
+        else {
+            this._sketch.appendChild(self);
+        }
+        this.self = self;
+    }
+    update(updateFeature) {
+        let box = this.self;
+        if (!box) {
+            throw new Error('Target box element can not be found');
+        }
+        if (updateFeature.width) {
+            this._width = updateFeature.width;
+            box.style.width = this._width + '%';
+        }
+        // color is more primary than getting random color with colorOptions
+        if (updateFeature.color) {
+            this._color = updateFeature.color;
+            box.style.backgroundColor = `rgba(${this._color[ColorOption.Red]}, ${this._color[ColorOption.Green]},${this._color[ColorOption.Blue]},${this._color[ColorOption.Alfa]})`;
+        }
+        if (updateFeature.classList) {
+            this._classList = updateFeature.classList;
+            const boxClassList = box.classList;
+            // Remove any class not present in the updated class list
+            for (const name of boxClassList) {
+                if (name !== 'sketch__box' && !this._classList.includes(name)) {
+                    boxClassList.remove(name);
+                }
+            }
+            // Add any new classes from the updated class list
+            for (const newName of this._classList) {
+                if (newName !== 'sketch__box' && !boxClassList.contains(newName)) {
+                    boxClassList.add(newName);
+                }
+            }
+        }
+    }
+}
+var BoxWidth;
+(function (BoxWidth) {
+    BoxWidth[BoxWidth["Small"] = 8] = "Small";
+    BoxWidth[BoxWidth["Medium"] = 16] = "Medium";
+    BoxWidth[BoxWidth["Large"] = 24] = "Large";
+})(BoxWidth || (BoxWidth = {}));
+class Sketch {
+    constructor(_self, _boxWidth = BoxWidth.Medium) {
+        this._self = _self;
+        this._boxWidth = _boxWidth;
+        this._boxes = [];
+    }
+    initBoxes() {
+        const sketchWidth = this._self.clientWidth;
+        const sketchHeight = this._self.clientHeight;
+        const row = this._boxWidth / sketchWidth;
+        const col = this._boxWidth / sketchHeight;
+        for (let i = 0; i < row; i++) {
+            this._boxes.push([]);
+            for (let j = 0; j < col; j++) {
+                const box = new Box(this._self, this._boxWidth);
+                box.add();
+                this._boxes[i].push(box);
+            }
+        }
+    }
+    deleteAllBox() {
+        for (let i = 0; i < this._boxes.length; i++) {
+            for (let j = 0; j < this._boxes[i].length; j++) {
+                const box = this._boxes[i][j].self;
+                if (!box) {
+                    throw new Error(`box position ${i - j} is not found`);
+                }
+                else {
+                    this._self.removeChild(box);
+                }
+            }
+        }
+    }
+    updateAllBoxColor(newColor) {
+        for (let i = 0; i < this._boxes.length; i++) {
+            for (let j = 0; j < this._boxes[i].length; j++) {
+                this._boxes[i][j].update({ color: newColor });
+            }
+        }
+    }
+    updateBoxWidth(width) {
+        this._boxWidth = width;
+        //add sa
+        this.deleteAllBox();
+        this.initBoxes();
+    }
+}
 //# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,148 @@
+enum ColorOption {
+    Red = 'red',
+    Green = 'green',
+    Blue = 'blue',
+    Alfa = 'alfa'
+}
+type Color = {
+    [ColorOption.Red]:number,
+    [ColorOption.Green]:number,
+    [ColorOption.Blue]:number,
+    [ColorOption.Blue]:number,
+    [ColorOption.Alfa]:number,
+}
+const defaultColor: Color = {
+    [ColorOption.Red]:255,
+    [ColorOption.Green]:255,
+    [ColorOption.Blue]:255,
+    [ColorOption.Alfa]: 1,
+}
+interface BoxFeature {
+    sketch: HTMLElement,
+    width: number,
+    color: Color,
+    classList:string[]
+}
+
+class Box {
+    public self: (HTMLElement|undefined) = undefined;
+
+    constructor(
+        private _sketch: HTMLElement,
+        private _width: number,
+        private _color: Color = JSON.parse(JSON.stringify(defaultColor)),
+        private _classList: string[] = []
+    ) {}
+
+    add(insertBeforeElement: (HTMLElement|undefined)=undefined): void {
+        let self = document.createElement('div');
+        //default className
+        self.classList.add('sketch__box');
+        //additional className for style changes
+        for(const name of this._classList) {
+            self.classList.add(name);
+        }
+        self.style.width = this._width + '%';
+        self.style.backgroundColor = `rgba(${this._color[ColorOption.Red]}, ${this._color[ColorOption.Green]},${this._color[ColorOption.Blue]},${this._color[ColorOption.Alfa]})`;
+        self.addEventListener('mousedown', () =>{
+            //some function to change color with specified color
+        });
+        if(insertBeforeElement) {
+            this._sketch.insertBefore(self, insertBeforeElement);
+        }else {
+             this._sketch.appendChild(self);
+        }
+        this.self = self;
+    }
+
+    update(updateFeature:Partial<Omit<BoxFeature, 'sketch'>>) {
+        let box = this.self
+        if(!box) {
+            throw new Error('Target box element can not be found')
+        }
+        if(updateFeature.width) {
+            this._width = updateFeature.width;
+            box.style.width = this._width + '%';
+        }
+        // color is more primary than getting random color with colorOptions
+        if(updateFeature.color) {
+            this._color = updateFeature.color;
+            box.style.backgroundColor = `rgba(${this._color[ColorOption.Red]}, ${this._color[ColorOption.Green]},${this._color[ColorOption.Blue]},${this._color[ColorOption.Alfa]})`;
+        }   
+        if(updateFeature.classList) {
+            this._classList = updateFeature.classList;
+            const boxClassList = box.classList;
+    
+            // Remove any class not present in the updated class list
+            for (const name of boxClassList) {
+                if (name !== 'sketch__box' && !this._classList.includes(name)) {
+                    boxClassList.remove(name);
+                }
+            }
+            
+            // Add any new classes from the updated class list
+            for (const newName of this._classList) {
+                if (newName !== 'sketch__box' && !boxClassList.contains(newName)) {
+                    boxClassList.add(newName);
+                }
+            }
+        }
+    }
+}
+enum BoxWidth {
+    Small = 8,
+    Medium = 16,
+    Large= 24
+}
+class Sketch {
+    private _boxes:Box[][] = [];
+    constructor(
+        private _self: HTMLElement,
+        private _boxWidth: BoxWidth = BoxWidth.Medium
+    ) {}
+
+    initBoxes() {
+        const sketchWidth = this._self.clientWidth;
+        const sketchHeight = this._self.clientHeight;
+        const row = this._boxWidth / sketchWidth;
+        const col = this._boxWidth / sketchHeight;
+
+        for (let i = 0; i < row; i++) {
+            this._boxes.push([]);
+            for (let j = 0; j < col; j++) {
+                const box = new Box(this._self, this._boxWidth);
+                box.add();
+                this._boxes[i].push(box);
+            }
+        }  
+    }
+
+    deleteAllBox() {
+        for(let i = 0; i < this._boxes.length; i++) {
+            for(let j = 0; j < this._boxes[i].length; j++){
+                const box = this._boxes[i][j].self;
+                if(!box) {
+                    throw new Error(`box position ${i-j} is not found`);
+                }else {
+                    this._self.removeChild(box);
+                }
+            }
+        }
+    }
+
+    updateAllBoxColor(newColor: Color) {
+        for(let i = 0; i < this._boxes.length; i++) {
+            for(let j = 0; j < this._boxes[i].length; j++){
+                this._boxes[i][j].update({color:newColor});
+            }
+        }
+    }
+
+    updateBoxWidth(width:BoxWidth) {
+        this._boxWidth = width;
+        //add sa
+        this.deleteAllBox();
+        this.initBoxes();
+    }
+
+}
